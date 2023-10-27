eb378ee92b57058f50dc0a44acc721cb45e7db46
Priyanshu Raj <priyanshurocky5@gmail.com>
2023-10-27 11:04:36 +0530

    added router for landing Page

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Route,Routes } from 'react-router-dom';
+import LandingPage from './components/Landing Page/LandingPage';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
