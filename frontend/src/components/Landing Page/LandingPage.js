9842f42ee5210bd9401b576973c0303d72b93803
Priyanshu Raj <priyanshurocky5@gmail.com>
2023-10-27 13:41:02 +0530

    heroSection component created

--- frontend/src/components/Landing Page/LandingPage.js
+++ frontend/src/components/Landing Page/LandingPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../Header/Header";
+import HeroSection from "../HeroSection/HeroSection";
 
 function LandingPage() {
   return (
     <div className="landing-page-container">
       <Header onPage="home" />
+      <HeroSection/>
     </div>
   );
 }
