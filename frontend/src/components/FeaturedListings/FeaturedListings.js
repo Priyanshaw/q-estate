23193c4c5fd4900cecc9af8370206426f0e32795
Priyanshu Raj <priyanshurocky5@gmail.com>
2023-10-28 13:50:52 +0530

    rendered cards and move assets to public folder

---
+++ frontend/src/components/FeaturedListings/FeaturedListings.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./FeaturedListings.css";
+import config from "../../config";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea, CardActions } from "@mui/material";
+
+export default function FeaturedListings() {
+  const [listingsData, setListingsData] = useState([]);
+
+  async function fetchListings() {
+    try {
+      let response = await axios.get(`${config.backEndPoint}/real-estate-data`);
+      // console.log(response);
+      let data = response.data.listings;
+      // console.log(data)
+      setListingsData(data.slice(0, 8));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchListings();
+  }, []);
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        {listingsData.length === 0 ? (
+          <Grid item>
+            <div classname="error-message">
+              <p>No Featured Listing found</p>
+            </div>
+          </Grid>
+        ) : (
+          listingsData.map((elem, index)=>(
+            <Grid item xs={12} sm={6} md={3} >
+            <Card sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={`/assets/real-estate-${index}.jpg`}
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {elem.property_name}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                    <div className="listings-detail">
+                        <span className="property-price">Rs {elem.price}</span>
+                        <span className="property-city">{elem.city}</span>
+                    </div>
+                </CardActions>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          ))
+        )}
+      </Grid>
+    </Box>
+  );
+}
--- frontend/src/components/Landing Page/LandingPage.js
+++ frontend/src/components/Landing Page/LandingPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../Header/Header";
 import HeroSection from "../HeroSection/HeroSection";
+import FeaturedListings from "../FeaturedListings/FeaturedListings";
 
 function LandingPage() {
   return (
     <div className="landing-page-container">
       <Header onPage="home" />
       <HeroSection/>
+      <FeaturedListings/>
     </div>
   );
 }
